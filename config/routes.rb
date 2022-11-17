7d93c6f5f6b5a2c1779b27160d48b310eac5e0a6
Emma ROSSO <emmarosso.pro@gmail.com>
2022-11-17 18:30:07 +0100

    MVC + routing + CSS

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # get "lists/:id", to: "lists#show", as: "list"
+  # post "lists", to: "lists#create"
+  # post "lists/:id/bookmarks"
+
+    resources :lists do
+      resources :bookmarks, only: [:create]
+    end
+    resources :bookmarks, only: :destroy
+  end
